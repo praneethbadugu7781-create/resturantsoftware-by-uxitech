621b3c9c7c03880e016b9e3b969c916cd10ce79f
Praneeth Badugu <praneethbadugu1530@gmail.com>
2026-07-31 21:43:33 +0530

    feat: Connect software database to new MongoDB Atlas cluster0.qos5v9x.mongodb.net

--- apps/web/app/api/v1/[...route]/route.ts
+++ apps/web/app/api/v1/[...route]/route.ts
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 // Set environment defaults for Vercel serverless functions
 if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = "mongodb+srv://badugupraneeth0_db_user:oS89myR4RBbTpTHL@cluster0.de9zaim.mongodb.net/uxitech_restaurant?retryWrites=true&w=majority&appName=Cluster0";
+  process.env.DATABASE_URL = "mongodb+srv://badugupraneeth0_db_user:oS89myR4RBbTpTHL@cluster0.qos5v9x.mongodb.net/uxitech_restaurant?retryWrites=true&w=majority&appName=Cluster0";
 }
 if (!process.env.JWT_SECRET) {
   process.env.JWT_SECRET = "change-me-access-secret";
